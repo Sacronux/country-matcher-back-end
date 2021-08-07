2ba477af3b44b6304be7b68d8439af0d9fbce479
Eugene Aleksieiev <sacronux@gmail.com>
2021-08-07 03:17:50 +0300

    chore(CM-10): update languages

--- controllers/userController.js
+++ controllers/userController.js
@@ -8,14 +8,14 @@ class userController {
 
       const payloadByToken = verifyAccessToken(req.headers.authorization)
       if (!payloadByToken) {
-        return res.status(403).json({ message: 'Authorization required' })
+        return res.status(403).json({ message: 'User updating error: authorization required, please try to login again' })
       }
 
       const newUsername = username ?? payloadByToken.username;
       const newCitizenship = citizenship ?? payloadByToken.citizenship;
       
       if (!newUsername) {
-        return res.status(400).json({ message: 'Authorization failed, try to logout and login again' })
+        return res.status(400).json({ message: 'User updating error: authorization failed, try to logout and login again' })
       }
 
       let potentialSameNameUser = null;
@@ -24,7 +24,7 @@ class userController {
         potentialSameNameUser = await User.findOne({ username })
       }
       if (potentialSameNameUser) {
-        return res.status(400).json({ message: 'User with same name is already exists' });
+        return res.status(400).json({ message: 'User updating error: user with same name is already exists' });
       }
 
       const currentUser = await User.findOneAndUpdate({ username: payloadByToken.username }, {
